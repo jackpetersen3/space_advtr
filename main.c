f7f78a1445a89879180b87dabab6d99bb48e4c7a
Jack Petersen <peterja7@os2.engr.oregonstate.edu>
2021-05-20 11:43:24 -0700

    refactores to have functions outside of main

--- main.c
+++ main.c
@@ -2,19 +2,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
+void getName(void);
+void planetLoop(void);
 int main(int argc, char *argv[])
 {
-    int flag = 0;
-    char name [30], input[10], planet[20];
+    getName();
+    planetLoop();
+    return 0;
+}
+
+void getName(void) {
+    char name[30];
     printf("Welcome to the solar system\n");
     printf("There are 9 planets to explore\n");
     printf("What is your name?\n");
     scanf("%s", name );
     printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa\n", name);
     printf("Let's go on an adventure\n");
+}
+
+void planetLoop(void) {
+    int flag = 0;
+    char input[30], planet[20];
     do {
         printf("Shall I randomly choose a planet for you to visit?\n");
+        sleep(1);
         scanf("%s", input);
         if(strcmp(input, "Y") == 0 || strcmp(input, "y") == 0) {
             printf("Traveling to Pluto...\n");
@@ -32,5 +46,5 @@ int main(int argc, char *argv[])
             printf("Sorry, I don't understand..\n");
         }
     } while(flag == 0);
-    return 0;
+
 }
